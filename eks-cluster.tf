a5a754f10217aa1dad8f4989789d7f5a1ad04597
Rodrigo De Vincenzo Monteiro <caprica64@icloud.com>
2021-09-30 19:56:41 +0000

    Test worker nodes changes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -45,7 +45,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t3.micro"
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
@@ -55,7 +55,7 @@ module "eks" {
       instance_type                 = "t2.micro"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = 4
     },
   ]
 }

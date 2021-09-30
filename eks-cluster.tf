7b35a05b03c4daac5ae011c7ea8de7fbae3c291e
Rodrigo De Vincenzo Monteiro <caprica64@icloud.com>
2021-09-30 21:22:53 +0000

    Remove Bottlerocket worker nodes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -61,37 +61,6 @@ module "eks" {
 }
 
 
-  worker_groups_launch_template = [
-    {
-      name                 = "bottlerocket-nodes"
-      ami_id               = data.aws_ami.bottlerocket_ami.id
-      instance_type        = "t3a.small"
-      asg_desired_capacity = 2
-      key_name             = aws_key_pair.nodes.key_name
-
-      # Since we are using default VPC there is no NAT gateway so we need to
-      # attach public ip to nodes so they can reach k8s API server
-      # do not repeat this at home (i.e. production)
-      public_ip = false
-
-      # This section overrides default userdata template to pass bottlerocket
-      # specific user data
-      userdata_template_file = "${path.module}/userdata.toml"
-      # we are using this section to pass additional arguments for
-      # userdata template rendering
-      userdata_template_extra_args = {
-        enable_admin_container   = false
-        enable_control_container = true
-        aws_region               = local.region
-      }
-      # example of k8s/kubelet configuration via additional_userdata
-      additional_userdata = <<EOT
-[settings.kubernetes.node-labels]
-ingress = "allowed"
-EOT
-    }
-  ]
-
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id

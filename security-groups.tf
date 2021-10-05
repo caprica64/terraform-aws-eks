946e229ef30fc5ac5fb4351170568ae77aa6ffec
Rodrigo De Vincenzo Monteiro <caprica64@icloud.com>
2021-10-05 14:09:38 +0000

    Quick update and comments on the Security Group TF file commenting and explaining how to use when we have a previously created VPC

--- security-groups.tf
+++ security-groups.tf
@@ -1,49 +1,50 @@
+##
+## The following two SG are commented out as example to use but not needed in this lab. They are mainly used when creating the whole VPC within Terraform workspace. Note the vpc_id pointing to the VPC module, unused at this time.
+##
 
-resource "aws_security_group" "worker_group_mgmt_one" {
-  name_prefix = "worker_group_mgmt_one"
-  #vpc_id      = module.vpc.vpc_id
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "worker_group_mgmt_one" {
+#   name_prefix = "worker_group_mgmt_one"
+#   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+#   ingress {
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
 
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
+#     cidr_blocks = [
+#       "10.0.0.0/8",
+#     ]
+#   }
   
-  tags = {
-    Name = "WorkerNode1-MGNT"
-  }  
+#   tags = {
+#     Name = "WorkerNode1-MGNT"
+#   }  
   
-}
+# }
 
-resource "aws_security_group" "worker_group_mgmt_two" {
-  name_prefix = "worker_group_mgmt_two"
-  #vpc_id      = module.vpc.vpc_id
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "worker_group_mgmt_two" {
+#   name_prefix = "worker_group_mgmt_two"
+#   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+#   ingress {
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
 
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
+#     cidr_blocks = [
+#       "192.168.0.0/16",
+#     ]
+#   }
 
-  tags = {
-  Name = "WorkerNode2-MGNT"
-  }  
+#   tags = {
+#   Name = "WorkerNode2-MGNT"
+#   }  
   
-}
+# }
 
 resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
-  #vpc_id      = module.vpc.vpc_id
+  #vpc_id      = module.vpc.vpc_id  ## In this SG we use a previous created VPC so referencing the VPC module is and cannot be used here. We use the variable below to point to a static VPC id.
   vpc_id      = var.vpc_id
 
   ingress {

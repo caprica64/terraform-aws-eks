23e0ba7d3f22a8743e7b675f2ca2aea179a2e7df
Rodrigo De Vincenzo Monteiro <caprica64@icloud.com>
2021-10-05 14:03:31 +0000

    Try adding spot instance in node group 1

--- lauch_template.tf
+++ lauch_template.tf
@@ -18,10 +18,11 @@
 # Trivia: AWS transparently creates a copy of your LaunchTemplate and actually uses that copy then for the node group. If you DONT use a custom AMI,
 # then the default user-data for bootstrapping a cluster is merged in the copy.
 resource "aws_launch_template" "default" {
-  #name_prefix            = "eks-example-as-resource"
+  #name_prefix            = "eks-example-as-resource" ### <<- You can use name prefix and have a computed sufix for your worknode name.
   name                   = "LaunchTemplate1"
   description            = "Default Launch-Template as resource"
   update_default_version = true
+  kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot"
 
   block_device_mappings {
     device_name = "/dev/xvda"

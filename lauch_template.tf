de46c031fb578f357287262b9ee656461b170c5d
Rodrigo De Vincenzo Monteiro <caprica64@icloud.com>
2021-10-05 11:13:19 +0000

    Test seconday LT

--- lauch_template.tf
+++ lauch_template.tf
@@ -104,3 +104,97 @@ resource "aws_launch_template" "default" {
     create_before_destroy = true
   }
 }
+
+
+
+
+
+
+
+resource "aws_launch_template" "secondary" {
+  #name_prefix            = "eks-example-as-resource"
+  name                   = "LaunchTemplate2"
+  description            = "Default Launch-Template as resource"
+  update_default_version = true
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = 32
+      volume_type           = "gp3"
+      delete_on_termination = true
+      # encrypted             = true
+
+      # Enable this if you want to encrypt your node root volumes with a KMS/CMK. encryption of PVCs is handled via k8s StorageClass tho
+      # you also need to attach data.aws_iam_policy_document.ebs_decryption.json from the disk_encryption_policy.tf to the KMS/CMK key then !!
+      # kms_key_id            = var.kms_key_arn
+    }
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    delete_on_termination       = true
+    security_groups             = [module.eks.worker_security_group_id, aws_security_group.all_worker_mgmt.id]  #Example of use of standard SG from EKS module and a customised SG from security_groups.tf file
+  }
+
+  # if you want to use a custom AMI
+  # image_id      = var.ami_id
+
+  # If you use a custom AMI, you need to supply via user-data, the bootstrap script as EKS DOESNT merge its managed user-data then
+  # you can add more than the minimum code you see in the template, e.g. install SSM agent, see https://github.com/aws/containers-roadmap/issues/593#issuecomment-577181345
+  #
+  # (optionally you can use https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/cloudinit_config to render the script, example: https://github.com/terraform-aws-modules/terraform-aws-eks/pull/997#issuecomment-705286151)
+
+  # user_data = base64encode(
+  #   data.template_file.launch_template_userdata.rendered,
+  # )
+
+
+  # Supplying custom tags to EKS instances is another use-case for LaunchTemplates
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      CustomTag = "EKS example"
+      Name = "Worker Node group Default"
+      Environment = "Dev"
+      Project     = "EKS"
+    }
+  }
+
+  # Supplying custom tags to EKS instances root volumes is another use-case for LaunchTemplates. (doesnt add tags to dynamically provisioned volumes via PVC tho)
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = {
+      CustomTag   = "EKS example"
+      Environment = "Dev"
+      Project     = "EKS"
+      }
+  }
+
+  # Supplying custom tags to EKS instances ENI's is another use-case for LaunchTemplates
+  tag_specifications {
+    resource_type = "network-interface"
+
+    tags = {
+      CustomTag = "EKS example"
+    }
+  }
+
+  # Tag the LT itself
+  tags = {
+    CustomTag = "EKS example"
+    Project = "EKS"
+    Environment = "Dev"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}

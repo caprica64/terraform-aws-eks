15f45765d3743af3f9ed663248cad8ce21ec81f1
Rodrigo De Vincenzo Monteiro <63541433+caprica64@users.noreply.github.com>
2022-01-15 12:10:46 -0300

    Restore

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72.0"
+      version = ">= 3.20.0"
     }
 
     random = {
@@ -26,6 +26,6 @@ terraform {
     }
   }
 
-  required_version = "> 1.0"
+  required_version = "> 0.14"
 }
 
